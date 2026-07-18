5696b64fa62e68688d20d2d7f2e7d0256aafadf7
面条 <root@miantiao.me>
2026-07-19 00:43:29 +0800

    refactor: remove prerender configuration for root and dashboard routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,12 +49,6 @@ export default defineNuxtConfig({
     },
   },
   routeRules: {
-    '/': {
-      prerender: true,
-    },
-    '/dashboard/**': {
-      prerender: true,
-    },
     '/dashboard': {
       redirect: '/dashboard/links',
     },
